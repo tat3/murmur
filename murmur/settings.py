c41d1896e9d46900ec8bb6517d4192c1d63668ed
tat3 <n.tatsuki.214@gmail.com>
2018-04-17 07:34:10 +0900

    Add heroku domain to allowed hosts

--- murmur/settings.py
+++ murmur/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("PRODUCTION", "False") != "True"
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "murmur2.herokuapp.com"]
 
 
 # Application definition
